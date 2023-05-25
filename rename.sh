604d3214e8974326c9973b27badc7141ef547103
Brian Rice <brianrice013@gmail.com>
2023-05-25 10:05:59 -0500

    Make renaming easier

--- rename.sh
+++ rename.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
 set -euo pipefail
 
-for file in _posts/*-"$1".md; do
-    without_dir=${file#_posts/}
-    date=${without_dir:0:10}
-    new_file="_posts/${date}-$2.md"
-    mv $file $new_file
-    echo "Renamed $file to $new_file"
-done
+/usr/local/bin/rename "s/${1}/${2}/" _posts/*.md
